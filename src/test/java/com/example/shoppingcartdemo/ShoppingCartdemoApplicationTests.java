689136d8a33eea604b5f037a923cbe5cd15b96b4
longhoanglekim <115090953+longhoanglekim@users.noreply.github.com>
2024-07-09 00:10:49 +0700

    Add files

---
+++ src/test/java/com/example/shoppingcartdemo/ShoppingCartdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/main/java/com/example/shoppingcartdemo/ShoppingCartdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartdemoApplication.class, args);
+    }
+
+}
